3f8923bb9e3511d515b9b7b69c29b5d42660738d
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-21 00:00:49 -0800

    this is concept of sql of order by  which is sort the result set

---
+++ order.sql
@@ -0,0 +1,17 @@
+/*
+this is concept sort the result set in the ascending or descending
+*/
+
+select * from products order by price;
+
+select * from products order by price DESC;
+
+
+select * from products order by productName;
+
+select * from products order by productName DESC;
+
+select * from products order by country,customerName;
+
+select * from products order by country ASC,customerName DESC;
+
