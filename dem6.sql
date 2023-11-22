2e226eac75fda98e3a137761321ef71c98c78598
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-22 02:01:58 -0800

    this is used the one or more AND operator

---
+++ dem6.sql
@@ -0,0 +1,7 @@
+
+
+select * from customers where country='Germany' AND city='Berlin' AND postalcode>12000;
+
+
+
+//The following SQL statement selects all fields from Customers where Country is "Germany" AND City is "Berlin" AND PostalCode is higher than 12000:
