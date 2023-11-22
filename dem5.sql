7bd86e0ff237a74efccbb30a29f6d76c7787bf71
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-22 01:55:55 -0800

    this is AND operator used the sql query

---
+++ dem5.sql
@@ -0,0 +1,6 @@
+//AND operator
+
+select * from customers where country='spain' AND customerName LIKE 'G';
+
+
+//The AND operator is used to filter records based on more than one condition, like if you want to return all customers from Spain that starts with the letter 'G':
