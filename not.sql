ffc5e18736687a9b39bd53e32238fe644b58d52c
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-23 02:00:18 -0800

    In the select statement below we want to return all customers that are NOT from Spain:

---
+++ not.sql
@@ -0,0 +1,5 @@
+select * from customers where country <>'spain'
+
+select * from customers where NOT country='spain'
+
+//In the select statement below we want to return all customers that are NOT from Spain:
