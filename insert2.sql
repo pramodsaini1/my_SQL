bb13952a793ffcabf36e4fa4177008f3ccd11a7d
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-24 01:47:48 -0800

    insert the record on the databse into the multiple column

---
+++ insert2.sql
@@ -0,0 +1,4 @@
+insert into customers (customerName,city,contact,PostalCode) values
+('pk','sikar','8905648522','332222'),
+('dk','jaipur','9929046899','121212'),
+('sk','sridham','2323232323','1212322');
