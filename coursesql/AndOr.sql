fc7085c93a5f8024574d59eec85ff13a4800d3ca
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-30 03:17:44 -0800

    here the concept of the or,and ,not sql qiery

---
+++ coursesql/AndOr.sql
@@ -0,0 +1,10 @@
+select * from person where Age>=18 AND Age<=21
+
+select * from person where Age>=18 OR Age<=21
+
+select * from person where age<=20 AND city="agra"
+
+select * from person where city="agra" OR city ="'bhopal"  AND gender="female"
+
+select * from person where city NOT="agra" OR city="bhopal"  
+
